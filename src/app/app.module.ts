d09254defddf310e345f0d17959ea6887c49d01d
sivakumar-sagi <Sivakumar.Sagi@openlane.com>
2018-04-24 19:33:36 +0530

    Added real data to display the chart...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BellCurveChartComponent } from '../app/bell-curve-chart/bell-curve-chart.component';
 
@@ -10,7 +10,8 @@ import { BellCurveChartComponent } from '../app/bell-curve-chart/bell-curve-char
     BellCurveChartComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/bell-curve-chart/bell-curve-chart.component.ts
+++ src/app/bell-curve-chart/bell-curve-chart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 declare var Highcharts;
 @Component({
   selector: 'app-bell-curve-chart',
@@ -6,52 +7,100 @@ declare var Highcharts;
   styleUrls: ['./bell-curve-chart.component.css']
 })
 export class BellCurveChartComponent implements OnInit {
-
-  constructor() { }
+  candidateScoreList = [];
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit() {
-  	var data = [48.214285714285715, 42.857142857142854, 85.71428571428572, 41.07142857142857, 39.285714285714285, 73.21428571428572];
-  	Highcharts.chart('container', {
-	    title: {
-	        text: 'Bell curve'
-	    },
-
-	    xAxis: [{
-	        title: {
-	            text: 'Data'
-	        },
-	        alignTicks: false
-	    }, {
-	        title: {
-	            text: 'Bell curve'
-	        },
-	        alignTicks: false,
-	        opposite: true
-	    }],
-
-	    yAxis: [{
-	        title: { text: 'Data' }
-	    }, {
-	        title: { text: 'Bell curve' },
-	        opposite: true
-	    }],
-
-	    series: [{
-	        name: 'Bell curve',
-	        type: 'bellcurve',
-	        xAxis: 1,
-	        yAxis: 1,
-	        baseSeries: 1,
-	        zIndex: -1
-	    }, {
-	        name: 'Data',
-	        type: 'scatter',
-	        data: data,
-	        marker: {
-	            radius: 1.5
-	        }
-	    }]
-	});
+  	this.httpClient.get('https://api.myjson.com/bins/7azib').subscribe(
+  		(data: any)=>{
+  			for(var i=0;i<data.candidateScoreList.length;i++){
+  				this.candidateScoreList.push(data.candidateScoreList[i].totalScore);
+  			}
+  			console.log(this.candidateScoreList);
+  		}
+  	);
+  	// if(this.candidateScoreList.length>0){
+  		var data =  [
+		    48.214285714285715,
+		    42.857142857142854,
+		    19.642857142857142,
+		    46.42857142857143,
+		    73.21428571428572,
+		    85.71428571428572,
+		    39.285714285714285,
+		    14.285714285714285,
+		    80.35714285714286,
+		    41.07142857142857,
+		    66.07142857142857,
+		    73.21428571428572,
+		    85.71428571428572,
+		    66.07142857142857,
+		    73.21428571428572,
+		    39.285714285714285,
+		    71.42857142857143,
+		    41.07142857142857,
+		    71.42857142857143,
+		    49.07142857142857,
+		    25,
+		    67.85714285714286,
+		    53.57142857142857,
+		    41.07142857142857,
+		    60.714285714285715,
+		    85.71428571428572,
+		    80.35714285714286,
+		    41.07142857142857,
+		    78.57142857142857,
+		    19.642857142857142,
+		    78.57142857142857,
+		    33.92857142857143,
+		    46.42857142857143,
+		    66.07142857142857,
+		    46.42857142857143,
+		    46.42857142857143,
+		    25,
+		    53.57142857142857,
+		    58.92857142857143,
+		    85.71428571428572
+		  ];
+	  	Highcharts.chart('container', {
+		    title: {
+		        text: 'Overall assessement score 49%'
+		    },
+		    xAxis: [{
+		        title: {
+		            text: 'Score in %'
+		        },
+		        alignTicks: false
+		    }, {
+		        title: {
+		            text: ''
+		        },
+		        alignTicks: false,
+		        opposite: true
+		    }],
+		    yAxis: [{
+		        title: { text: 'No of candidates' }
+		    }, {
+		        title: { text: '' },
+		        opposite: true
+		    }],
+		    series: [{
+		        name: 'Bell curve',
+		        type: 'bellcurve',
+		        xAxis: 1,
+		        yAxis: 1,
+		        baseSeries: 1,
+		        zIndex: -1
+		    }, {
+		        name: 'Data',
+		        type: 'scatter',
+		        data: data,
+		        marker: {
+		            radius: 1.5
+		        }
+		    }]
+		});
+  	// }
   }
 
 }
